d8c036f9843731c2965d868e39d20c02a74a59ae
Owen Jones <owen@owenjones.net>
2020-06-26 18:51:05 +0100

    some basic utility functions

---
+++ site/utils.py
@@ -0,0 +1,20 @@
+from flask import current_app
+from os import path
+from urllib.parse import urlparse, urljoin
+
+
+def isSafeUrl(target, request):
+    ref_url = urlparse(request.host_url)
+    test_url = urlparse(urljoin(request.host_url, target))
+    return test_url.scheme in ("http", "https") and ref_url.netloc == test_url.netloc
+
+
+def allowedFile(filename):
+    return (
+        "." in filename
+        and getFileExtension(filename) in current_app.config["UPLOAD_EXTENSIONS"]
+    )
+
+
+def getFileExtension(filename):
+    return filename.rsplit(".", 1)[1].lower()
