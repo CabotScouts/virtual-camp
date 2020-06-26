2ec84894b66949d4ec5ee6ada10a68852f929320
Owen Jones <owen@owenjones.net>
2020-06-26 18:43:29 +0100

    added flags to Share model, made Group model derive from User auth funcs

--- site/models.py
+++ site/models.py
@@ -5,8 +5,6 @@ from CabotAtHome.site import db, login_manager
 
 
 class User(UserMixin, db.Model):
-    __tablename__ = "users"
-
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), unique=True)
     key = db.Column(db.Integer)
@@ -16,21 +14,23 @@ class User(UserMixin, db.Model):
 
 
 class Share(db.Model):
-    __tablename__ = "shares"
-
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50))
     group_id = db.Column(db.Integer, db.ForeignKey("group.id"), nullable=False)
     file = db.Column(db.String(120), nullable=False)
     comment = db.Column(db.Text)
+    approved = db.Column(db.Boolean, default=False)
+    flagged = db.Column(db.Boolean, default=False)
+    gallery = db.Column(db.Boolean, default=False)
 
     def __repr__(self):
         return f"<Share {self.id}>"
 
+    def approve(self):
+        pass
 
-class Group(db.Model):
-    __tablename__ = "groups"
 
+class Group(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50))
     key = db.Column(db.Integer)
