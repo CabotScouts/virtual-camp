df78ff1007d584fa165499cb67be344684d06144
Owen Jones <owen@owenjones.net>
2020-06-27 00:17:34 +0100

    added some test db data

--- site/__init__.py
+++ site/__init__.py
@@ -5,13 +5,30 @@ from flask_login import LoginManager
 
 from CabotAtHome.site.controllers import registerControllers
 
-db = SQLAlchemy()
-login_manager = LoginManager()
-
 app = Flask(__name__)
 app.config.from_object("CabotAtHome.site.config.Config")
 
-db.init_app(app)
+db = SQLAlchemy(app)
+login_manager = LoginManager()
+
 login_manager.init_app(app)
+
+## TEST DB DATA
+from CabotAtHome.site.models import User, Group, Share
+
+db.create_all()
+
+groups = ["1st Bishopston", "7th Bristol"]
+for group in groups:
+    g = Group(name=group)
+    db.session.add(g)
+
+users = [("owen", 1001)]
+for user in users:
+    u = User(name=user[0], key=user[1])
+    db.session.add(u)
+
+db.session.commit()
+## TEST DB DATA
 
 registerControllers(app)
