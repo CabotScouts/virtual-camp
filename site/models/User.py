fba27fb0e833bb6676b9427cca1e6762d1b11487
Owen Jones <owen@owenjones.net>
2020-06-27 00:23:00 +0100

    changed key column from int to string

--- site/models/User.py
+++ site/models/User.py
@@ -6,7 +6,7 @@ from CabotAtHome.site import db, login_manager
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), unique=True)
-    key = db.Column(db.Integer)
+    key = db.Column(db.String(10))
 
     def __repr__(self):
         return f"<User {self.id}>"
