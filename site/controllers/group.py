1e3420dcb15669c5c7900eb28f23689ae5870755
Owen Jones <owen@owenjones.net>
2020-06-27 17:21:40 +0100

    can now login as a group

--- site/controllers/group.py
+++ site/controllers/group.py
@@ -1,15 +1,26 @@
-from flask import Blueprint, request, render_template
-from flask_login import login_user, logout_user
+from flask import Blueprint, request, render_template, flash, redirect, url_for
+from flask_login import login_required, login_user, logout_user, current_user
 
+from CabotAtHome.site import app
 from CabotAtHome.site.models import Group
+from CabotAtHome.site.models.User import Permission
+from CabotAtHome.site.auth import needs_group
 
-blueprint = Blueprint("group", __name__)
-prefix = "/group"
+blueprint = Blueprint("group", __name__, url_prefix="/group")
 
 
-@blueprint.route("/")
+@app.context_processor
+def injectAuthChecks():
+    def hasGroup():
+        return current_user.hasPermission(Permission.GROUP)
+
+    return dict(hasGroup=hasGroup)
+
+
+@blueprint.route("", strict_slashes=False)
+@needs_group
 def index():
-    pass
+    return render_template("group/index.jinja", group=current_user.group)
 
 
 @blueprint.route("/login")
@@ -20,7 +31,15 @@ def login():
 
 @blueprint.route("/login", methods=["POST"])
 def processLogin():
-    pass
+    g = Group.query.filter_by(id=request.form["group"]).first()
+    if g and (g.user.key == request.form["key"]):
+        login_user(g.user)
+        flash("Successfully logged in", "success")
+        return redirect(url_for("group.index"))
+
+    else:
+        flash("Username or key incorrect", "danger")
+        return redirect(url_for("group.login"))
 
 
 @blueprint.route("/logout")
