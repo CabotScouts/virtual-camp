fe824f0e8db577e11f1ce9d9c700f7a36e4b21fb
Owen Jones <owen@owenjones.net>
2020-06-27 17:23:38 +0100

    started adding management functions

--- site/controllers/manage.py
+++ site/controllers/manage.py
@@ -1,9 +1,10 @@
 from flask import Blueprint, request, flash, render_template, redirect, url_for
+from flask_login import current_user
 
 from CabotAtHome.site.auth import needs_manage, needs_admin
 from CabotAtHome.site.models import User, Group, Share
 
-blueprint = Blueprint("admin", __name__, url_prefix="/admin")
+blueprint = Blueprint("manage", __name__, url_prefix="/manage")
 
 
 @blueprint.route("", strict_slashes=False)
@@ -17,5 +18,34 @@ def index():
 @blueprint.route("/users")
 @needs_admin
 def users():
-    users = User.query.all()
+    users = User.query.filter_by(group=None)
     return render_template("admin/users.jinja", users=users)
+
+
+@blueprint.route("/users/add", methods=["POST"])
+@needs_admin
+def addUser():
+    users = User.query.filter_by(group=None)
+    return redirect(url_for("manage.users"))
+
+
+@blueprint.route("users/regenerate-key/<int:id>")
+@needs_admin
+def regenerateKey(id):
+    user = User.query.filter_by(id=id).first()
+    if not user:
+        flash(f"Unknown user <#{id}>", "danger")
+        return redirect(url_for("manage.users"))
+
+    if user.id == current_user.id:
+        flash("You can't regenerate your own key", "danger")
+        return redirect(url_for("manage.users"))
+
+    return redirect(url_for("manage.users"))
+
+
+@blueprint.route("/groups")
+@needs_manage
+def groups():
+    groups = Group.query.all()
+    return render_template("admin/groups.jinja", groups=groups)
