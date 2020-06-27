250e57da038da57e3f4e7bf7d0f89c8f5e6e746c
Owen Jones <owen@owenjones.net>
2020-06-27 01:37:57 +0100

    fixed Group->User relationship, group seeding now adds a group-user

--- site/models/Group.py
+++ site/models/Group.py
@@ -1,18 +1,18 @@
-from CabotAtHome.site import db, login_manager
-from CabotAtHome.site.utils import randomKey
+from CabotAtHome.site import db
+from CabotAtHome.site.models import User
 
 
 class Group(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50))
-    key = db.Column(db.Integer)
 
     def __repr__(self):
         return f"<Group { self.name }>"
 
     def __init__(self, **kwargs):
         super(Group, self).__init__(**kwargs)
-        self.generateKey()
+        self.user = User(name=self.sanitisedName)
 
-    def generateKey(self):
-        self.key = randomKey(6)
+    @property
+    def sanitisedName(self):
+        return self.name.lower().replace(" ", "-")
--- site/models/User.py
+++ site/models/User.py
@@ -1,12 +1,36 @@
+from enum import Enum
 from flask_login import UserMixin
 
 from CabotAtHome.site import db, login_manager
+from CabotAtHome.site.utils import randomKey
+
+
+class UserType(Enum):
+    USER = 0x0001
+    GROUP = 0x0010
+    ADMIN = 0x0100
 
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), unique=True)
-    key = db.Column(db.String(10))
+    key = db.Column(db.String(6))
+
+    type = db.Column(db.Enum(UserType), nullable=False, default=UserType.USER)
+
+    group_id = db.Column(db.Integer, db.ForeignKey("group.id"))
+    group = db.relationship(
+        "Group", backref=db.backref("user", lazy=True, uselist=False),
+    )
 
     def __repr__(self):
-        return f"<User {self.id}>"
+        return f"<User ({self.id}, {self.name}, {self.type})>"
+
+    def __init__(self, **kwargs):
+        super(User, self).__init__(**kwargs)
+
+        if "key" not in kwargs:
+            self.generateKey()
+
+    def generateKey(self):
+        self.key = randomKey(6)
