836f0adce1aaf7a5493aba5d4a31cdc2cb69404e
Owen Jones <owen@owenjones.net>
2020-06-28 03:56:52 +0100

    added CSRF protection

--- site/__init__.py
+++ site/__init__.py
@@ -2,6 +2,7 @@ import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from flask_wtf.csrf import CSRFProtect
 
 from CabotAtHome.site.controllers import registerControllers
 
@@ -9,9 +10,10 @@ app = Flask(__name__)
 app.config.from_object("CabotAtHome.site.config.Config")
 
 db = SQLAlchemy(app)
-login_manager = LoginManager()
+login_manager = LoginManager(app)
+csrf = CSRFProtect(app)
 
-login_manager.init_app(app)
+# login_manager.init_app(app)
 # login_manager.session_protection = "strong"
 
 from CabotAtHome.site.seed import seed
