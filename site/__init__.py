195aa2936640d4a57fd874455045235ac0a81a84
Owen Jones <owen@owenjones.net>
2020-06-28 03:43:42 +0100

    initial seed file

--- site/__init__.py
+++ site/__init__.py
@@ -12,45 +12,10 @@ db = SQLAlchemy(app)
 login_manager = LoginManager()
 
 login_manager.init_app(app)
-login_manager.session_protection = "strong"
+# login_manager.session_protection = "strong"
 
-## TEST DB DATA
-from CabotAtHome.site.models import User, Group
-from CabotAtHome.site.models.User import Role
+from CabotAtHome.site.seed import seed
 
-db.create_all()
-
-u = User(username="root", key="1001", role=Role.ADMIN)
-db.session.add(u)
-
-groups = [
-    "1st Bishopston",
-    "3rd Bristol",
-    "4th Bristol (1st Southmead)",
-    "7th Bristol (Christ Church Clifton)",
-    "18th Bristol (1st Redland Green)",
-    "26th Bristol (North Cote)",
-    "43rd Bristol (St. Mary's Stoke Bishop)",
-    "44th Bristol (White Tree)",
-    "62nd Bristol (Horfield Methodist and Parish Church)",
-    "63rd Bristol (St. Andrew's with St. Bartholomew's)",
-    "77th Bristol (Redland Park)",
-    "90th Bristol (Westbury Methodists)",
-    "91st Bristol (Horfield Baptist)",
-    "126th Bristol (Sea Mills)",
-    "167th Bristol (Westbury Baptist)",
-    "169th Bristol (Brentry)",
-    "191st Bristol (St. Mary's Shirehampton)",
-    "227th Bristol (St. Peter's)",
-    "Bristol Royal Hospital for Children",
-    "Cabot Explorers",
-    "Gromit Network",
-]
-for group in groups:
-    g = Group(name=group)
-    db.session.add(g)
-
-db.session.commit()
-## TEST DB DATA
+seed()
 
 registerControllers(app)
