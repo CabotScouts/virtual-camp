35d38532165802e9417ba944cfbd52b808e92ba5
Owen Jones <owen@owenjones.net>
2020-07-01 23:47:48 +0100

    Added Flask-Talisman for security, and set CSP

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,18 @@
 import os
 
 from flask import Flask
+from flask_talisman import Talisman
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_wtf.csrf import CSRFProtect
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
 
+from app.config import ContentSecurityPolicy
+
+talisman = Talisman()
 db = SQLAlchemy()
 login_manager = LoginManager()
-login_manager.session_protection = "strong"
 csrf = CSRFProtect()
 limiter = Limiter(key_func=get_remote_address)
 
@@ -18,6 +21,7 @@ def create_app():
     app = Flask(__name__)
     app.config.from_object("app.config.Config")
 
+    talisman.init_app(app, content_security_policy=ContentSecurityPolicy)
     db.init_app(app)
     login_manager.init_app(app)
     csrf.init_app(app)
--- app/config.py
+++ app/config.py
@@ -1,7 +1,30 @@
+ContentSecurityPolicy = {
+    "default-src": [
+        "'self'",
+        "fonts.googleapis.com",
+        "fonts.gstatic.com",
+        "*.youtube.com",
+    ],
+    "img-src": ["*", "data:"],
+}
+
+
 class Config:
     SECRET_KEY = "cats"
 
+    MAX_CONTENT_LENGTH = 50 * 1024 * 1024
     UPLOAD_FOLDER = "app/uploads"
-    UPLOAD_EXTENSIONS = {"png", "jpg", "jpeg", "gif", "mov", "mp4", "avi"}
+    UPLOAD_EXTENSIONS = {
+        "bmp",
+        "gif",
+        "jpg",
+        "jpeg",
+        "png",
+        "webp",
+        "avi",
+        "mov",
+        "mp4",
+        "webm",
+    }
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
