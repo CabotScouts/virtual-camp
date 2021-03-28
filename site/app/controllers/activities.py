f65b8256fcd8926c41270a701721c46844ae7d08
Owen Jones <owen@owenjones.net>
2021-03-29 00:36:48 +0100

    more un-updated pages to hide

--- site/app/controllers/activities.py
+++ site/app/controllers/activities.py
@@ -10,26 +10,26 @@ from flask import (
 blueprint = Blueprint("activities", __name__, url_prefix="/activities")
 
 
-@blueprint.route("")
-def index():
-    return render_template("activities/index.jinja")
-
-
-@blueprint.route("beavers")
-def beavers():
-    return render_template("activities/beavers.jinja")
-
-
-@blueprint.route("cubs")
-def cubs():
-    return render_template("activities/cubs.jinja")
-
-
-@blueprint.route("scouts")
-def scouts():
-    return render_template("activities/scouts.jinja")
-
-
-@blueprint.route("explorers-network")
-def explorers():
-    return render_template("activities/explorers.jinja")
+# @blueprint.route("")
+# def index():
+#     return render_template("activities/index.jinja")
+#
+#
+# @blueprint.route("beavers")
+# def beavers():
+#     return render_template("activities/beavers.jinja")
+#
+#
+# @blueprint.route("cubs")
+# def cubs():
+#     return render_template("activities/cubs.jinja")
+#
+#
+# @blueprint.route("scouts")
+# def scouts():
+#     return render_template("activities/scouts.jinja")
+#
+#
+# @blueprint.route("explorers-network")
+# def explorers():
+#     return render_template("activities/explorers.jinja")
--- site/app/controllers/share.py
+++ site/app/controllers/share.py
@@ -28,59 +28,59 @@ blueprint = Blueprint("share", __name__, url_prefix="/share")
 #     groups = Group.query.all()
 #     return render_template("share/new.jinja", groups=groups)
 
-
-@blueprint.route("", methods=["POST"])
-def upload():
-    if "group" not in request.form:
-        flash(
-            "<i class=\"fas fa-exclamation-circle pr-2\"></i>You didn't tell us which Scout Group you're in!",
-            "danger",
-        )
-        return redirect(request.url)
-
-    group = Group.query.filter_by(id=request.form["group"]).first()
-
-    if not group:
-        flash(
-            '<i class="fas fa-exclamation-circle pr-2"></i>There\'s something wrong with the Group you chose, please try to share your file again',
-            "danger",
-        )
-        return redirect(request.url)
-
-    if "file" not in request.files or request.files["file"].filename == "":
-        flash(
-            '<i class="fas fa-exclamation-circle pr-2"></i>You didn\'t chose a picture or video file to share with us',
-            "danger",
-        )
-        return redirect(request.url)
-
-    file = request.files["file"]
-    valid = allowedFile(file.filename)
-
-    if not valid:
-        flash(
-            "<i class=\"fas fa-exclamation-circle pr-2\"></i>The file you've tried to share isn't a valid picture or video file",
-            "danger",
-        )
-        return redirect(request.url)
-
-    if file and valid:
-        s = Share(
-            name=request.form["name"],
-            ip=request.remote_addr,
-            comment=request.form["comment"],
-            group_id=group.id,
-            ext=getFileExtension(file.filename),
-        )
-        file.save(os.path.join(current_app.config["UPLOAD_FOLDER"], s.file))
-        db.session.add(s)
-        db.session.commit()
-
-        flash(
-            '<i class="fas fa-paper-plane pr-2"></i>Your picture or video has been saved, keep your eyes peeled for it on the live stream and in our <span class="wow">Way Out West!</span> gallery!',
-            "success",
-        )
-        return redirect(url_for("share.new"))
+#
+# @blueprint.route("", methods=["POST"])
+# def upload():
+#     if "group" not in request.form:
+#         flash(
+#             "<i class=\"fas fa-exclamation-circle pr-2\"></i>You didn't tell us which Scout Group you're in!",
+#             "danger",
+#         )
+#         return redirect(request.url)
+#
+#     group = Group.query.filter_by(id=request.form["group"]).first()
+#
+#     if not group:
+#         flash(
+#             '<i class="fas fa-exclamation-circle pr-2"></i>There\'s something wrong with the Group you chose, please try to share your file again',
+#             "danger",
+#         )
+#         return redirect(request.url)
+#
+#     if "file" not in request.files or request.files["file"].filename == "":
+#         flash(
+#             '<i class="fas fa-exclamation-circle pr-2"></i>You didn\'t chose a picture or video file to share with us',
+#             "danger",
+#         )
+#         return redirect(request.url)
+#
+#     file = request.files["file"]
+#     valid = allowedFile(file.filename)
+#
+#     if not valid:
+#         flash(
+#             "<i class=\"fas fa-exclamation-circle pr-2\"></i>The file you've tried to share isn't a valid picture or video file",
+#             "danger",
+#         )
+#         return redirect(request.url)
+#
+#     if file and valid:
+#         s = Share(
+#             name=request.form["name"],
+#             ip=request.remote_addr,
+#             comment=request.form["comment"],
+#             group_id=group.id,
+#             ext=getFileExtension(file.filename),
+#         )
+#         file.save(os.path.join(current_app.config["UPLOAD_FOLDER"], s.file))
+#         db.session.add(s)
+#         db.session.commit()
+#
+#         flash(
+#             '<i class="fas fa-paper-plane pr-2"></i>Your picture or video has been saved, keep your eyes peeled for it on the live stream and in our <span class="wow">Way Out West!</span> gallery!',
+#             "success",
+#         )
+#         return redirect(url_for("share.new"))
 
 
 @blueprint.errorhandler(413)
@@ -116,15 +116,16 @@ def get(image):
         abort(403)
 
 
-@blueprint.route("/gallery")
-@blueprint.route("/gallery/<int:page>")
-def gallery(page=0):
-    shares = (
-        Share.query.filter_by(approved=True, featured=True)
-        .order_by(Share.id.desc())
-        .paginate(page, 20, False)
-    )
-    return render_template("share/gallery.jinja", shares=shares)
+#
+# @blueprint.route("/gallery")
+# @blueprint.route("/gallery/<int:page>")
+# def gallery(page=0):
+#     shares = (
+#         Share.query.filter_by(approved=True, featured=True)
+#         .order_by(Share.id.desc())
+#         .paginate(page, 20, False)
+#     )
+#     return render_template("share/gallery.jinja", shares=shares)
 
 
 @blueprint.route("/featured/<int:id>")
