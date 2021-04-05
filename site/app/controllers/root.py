19f03ef6e1a79eab92a3b0356f5501c96b0bfb68
Owen Jones <owen@owenjones.net>
2021-04-05 22:14:27 +0100

    re-enabled main event routes for programme and live stream, added main menu links

--- site/app/controllers/root.py
+++ site/app/controllers/root.py
@@ -13,16 +13,16 @@ def injectLinks():
             ("Home", "The event homepage", url_for("root.index")),
             ("Sign Up", "Sign up for the event", url_for("root.register")),
             ("St. George's Trail", "St. George's trail", url_for("activities.trail")),
-            # (
-            #     "Camp Programme",
-            #     "The programme for the weekend",
-            #     url_for("root.programme"),
-            # ),
-            # (
-            #     "Gallery",
-            #     "A gallery of images and videos sent into the event",
-            #     url_for("share.gallery"),
-            # ),
+            (
+                "Camp Programme",
+                "The programme for the weekend",
+                url_for("root.programme"),
+            ),
+            (
+                "Gallery",
+                "A gallery of images and videos sent into the event",
+                url_for("share.gallery"),
+            ),
         ]
     )
 
@@ -42,26 +42,25 @@ def register():
     return render_template("root/registration.jinja")
 
 
-#
-# @blueprint.route("/programme")
-# def programme():
-#     n = datetime.now()
-#     dn = n.day
-#     s = {25: "sat", 26: "sun"}
-#     ds = s[dn] if dn in s else ""
-#     h = n.hour + 1
-#     m = n.minute
-#
-#     return render_template(
-#         "root/programme.jinja",
-#         now={
-#             "day": ds,
-#             "hour": h,
-#             "min": m,
-#         },
-#     )
-#
-#
-# @blueprint.route("/live")
-# def live():
-#     return render_template("root/live.jinja")
+@blueprint.route("/programme")
+def programme():
+    n = datetime.now()
+    dn = n.day
+    s = {25: "sat", 26: "sun"}
+    ds = s[dn] if dn in s else ""
+    h = n.hour + 1
+    m = n.minute
+
+    return render_template(
+        "root/programme.jinja",
+        now={
+            "day": ds,
+            "hour": h,
+            "min": m,
+        },
+    )
+
+
+@blueprint.route("/live")
+def live():
+    return render_template("root/live.jinja")
